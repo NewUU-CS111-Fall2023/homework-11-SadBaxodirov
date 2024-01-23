4d73b22fd0a590fbc52cb0cf3b3394f31478172d
Sayidabdullakhon <127374049+SadBaxodirov@users.noreply.github.com>
2024-01-23 22:21:03 +0500

    Update task_5.cpp

--- src/cpp/task_5.cpp
+++ src/cpp/task_5.cpp
@@ -3,6 +3,31 @@
  * Date:
  * Name:
  */
-
+#include <unordered_map>
 class Problem5 {
+public:
+    string problem5(string key, string message) {
+        unordered_map<char, char> substitutionTable;
+        string alphabet = "abcdefghijklmnopqrstuvwxyz";
+        int index = 0;
+        for (char ch : key) {
+            if (ch != ' ' && substitutionTable.find(ch) == substitutionTable.end()) {
+                substitutionTable[ch] = alphabet[index++];
+            }
+        }
+        for (char ch : alphabet) {
+            if (substitutionTable.find(ch) == substitutionTable.end()) {
+                substitutionTable[ch] = alphabet[index++];
+            }
+        }
+        string decodedMessage;
+        for (char ch : message) {
+            if (ch == ' ') {
+                decodedMessage += ' ';
+            } else {
+                decodedMessage += substitutionTable[ch];
+            }
+        }
+        return decodedMessage;
+    }
 };
