61f70ccf89066b43069a85ee3582dd202e593e29
Sayidabdullakhon <127374049+SadBaxodirov@users.noreply.github.com>
2024-01-23 22:21:15 +0500

    Update task_6.cpp

--- src/cpp/task_6.cpp
+++ src/cpp/task_6.cpp
@@ -6,4 +6,32 @@
 
 class Problem6 {
 public:
+    bool isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i * i <= num; ++i) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    void problem6(int n, int& prime1, int& prime2) {
+        while (true) {
+            n++;
+            if (isPrime(n)) {
+                prime1 = n;
+                break;
+            }
+        }
+        while (true) {
+            n++;
+            if (isPrime(n)) {
+                prime2 = n;
+                break;
+            }
+        }
+    }
 };
