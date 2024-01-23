df6744d0ed41868b83f49e3a97dc3a36bbf8a393
Sayidabdullakhon <127374049+SadBaxodirov@users.noreply.github.com>
2024-01-23 22:20:50 +0500

    Update task_4.cpp

--- src/cpp/task_4.cpp
+++ src/cpp/task_4.cpp
@@ -5,4 +5,23 @@
  */
 
 class Problem4 {
+    const int base = 1337;
+private:
+    int modPow(int x, int k) {
+        int result = 1;
+        x %= base;
+        for (int i = 0; i < k; ++i) {
+            result = (result * x) % base;
+        }
+        return result;
+    }
+public:
+    int power(int a, vector<int> b) {
+        a %= base;
+        int result = 1;
+        for (int digit : b) {
+            result = (modPow(result, 10) * modPow(a, digit)) % base;
+        }
+        return result;
+    }
 };
