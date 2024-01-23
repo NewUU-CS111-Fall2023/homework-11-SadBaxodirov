0574657591d69943bda8d5ee8f052ad52f355fb1
Sayidabdullakhon <127374049+SadBaxodirov@users.noreply.github.com>
2024-01-23 22:20:12 +0500

    Update task_1.cpp

--- src/cpp/task_1.cpp
+++ src/cpp/task_1.cpp
@@ -3,7 +3,17 @@
  * Date:
  * Name:
  */
-
+#include <vector>
+using namespace std;
 class Problem1 {
 public:
+    int problem1(const vector<int>& nums, int target, int index, int currentSum) {
+        if (index == nums.size()) {
+            return (currentSum == target) ? 1 : 0;
+        }
+        int waysWithPlus = problem1(nums, target, index + 1, currentSum + nums[index]);
+        int waysWithMinus = problem1(nums, target, index + 1, currentSum - nums[index]);
+        return waysWithPlus + waysWithMinus;
+    }
+
 };
